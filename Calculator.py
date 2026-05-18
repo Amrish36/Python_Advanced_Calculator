a0ad723382e17cbde6dd209ec82fdac34dcc956a
Amrish36 <amrishsenthur36@gmail.com>
2026-05-18 12:59:49 +0530

    #Make it advanced next time!

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+#This is a basic calculator , make it advanced next time
+memory_answer = None
+
+while True:
+    print("---new_calculation---")
+    if memory_answer is not None:
+        print(f"last calculated answer in memory: {memory_answer}")
+    user_choice = input("enter start to start calculation or exit to exit: ").lower()
+    
+    if user_choice == "exit":
+        print("goodbye")
+        break  
+    elif user_choice == "start":
+        try:
+            var_1 = float(input("Enter a number  "))
+            basic_operator = input("enter any of these operators:+ , - , * , /,^.   ")
+            var_2 = float(input("Enter a second number  "))
+       
+    
+            if basic_operator == "+":
+                memory_answer = (var_1 + var_2)
+                print(memory_answer)  
+            elif basic_operator == "-": 
+                memory_answer = (var_1 - var_2)
+                print(memory_answer)
+            elif basic_operator == "*":
+                memory_answer = (var_1 * var_2)
+                print(memory_answer)
+            elif basic_operator == "/":
+                if var_2 == 0:
+                    print("You can\'t divide with 0!")
+                else:
+                    memory_answer = (var_1/var_2)
+                    print(memory_answer)
+            elif basic_operator == "^":
+                
+                memory_answer = (pow(var_1, var_2))
+                
+                print(memory_answer)
+            else:
+                print("Invalid operator")
+        except:
+            print("enter valid numbers only!")
